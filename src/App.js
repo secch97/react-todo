5607813cf6872c6c45b2fd5d32920bff6e0e7626
Saul Castillo <secch97@gmail.com>
2023-03-11 18:53:24 -0500

    Creation of a basic react component with JSX

--- src/App.js
+++ src/App.js
@@ -2,16 +2,13 @@ import React from 'react';
 
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    /* Fragment creation */
+    <>
+      <h1>Todo List</h1>
+      <ul>
+        
+      </ul>
+    </>
   );
 }
 
