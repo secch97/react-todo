c9044acd660efddd083075686a99475d7dec8690
Saul Castillo <secch97@gmail.com>
2023-03-16 20:57:01 -0400

    Created <Add Todo Form> Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import TodoList from './TodoList';
+import AddTodoForm from './AddTodoForm';
 
 const App = () => {
   return (
     /* Fragment creation */
     <>
       <h1>Todo List</h1>
+      <AddTodoForm/>
       <TodoList/>
     </>
   );
