8019f85c2b333440c160afb50f821da78d9c826a
Saul Castillo <secch97@gmail.com>
2023-03-23 15:42:39 -0400

    Handling of AddTodoForm submit event done

--- src/components/AddTodoForm.jsx
+++ src/components/AddTodoForm.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 
 const AddTodoForm = () => {
+  /*
+    ============================
+    =         HANDLERS         =
+    ============================
+  */
+
+  const handleAddTodo = (event) => {
+    event.preventDefault();
+    const todoTitle = event.target.title.value;
+    console.log(todoTitle);
+    event.target.reset();
+  };
+
   return (
-    <form>
+    <form onSubmit={handleAddTodo}>
         <label htmlFor="todoTitle">Title: </label>
-        <input id="todoTitle" type="text"/>
+        <input id="todoTitle" type="text" name="title"/>
         <button type="submit">Add</button>
     </form>
   );
