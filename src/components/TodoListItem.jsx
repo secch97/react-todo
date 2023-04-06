f097dc2a0754b6f20f9223c2acaf7053f287e31d
Saul Castillo <secch97@gmail.com>
2023-04-06 19:23:22 -0400

    Used useRef hook to animate listItem removal before removing it from the todos list

--- src/components/TodoListItem.jsx
+++ src/components/TodoListItem.jsx
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 // Third party libraries:
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const TodoListItem = ({id, title, onRemoveTodo}) => {
+
+  const [isRemoved, setIsRemoved] = useState(false);
+
+  const listItemRef = useRef();
+
+  useEffect(()=>{
+    if(isRemoved){
+      listItemRef.current.className="animate__animated animate__fadeOut animate__fast"
+      return;
+    }
+  }, [isRemoved]);
+
+  const handleRemoveTodo = () => {
+    setIsRemoved(true);
+    setTimeout(() => {
+      onRemoveTodo(id);
+    }, 1000);
+  }
+
   return (
-    <li className='animate__animated animate__backInUp animate__fast'>
+    <li ref={listItemRef} className='animate__animated animate__backInUp animate__fast'>
       {title}
       <button 
         className='button-icon'
-        onClick={() => onRemoveTodo(id)}
+        onClick={handleRemoveTodo}
       >
-        <FontAwesomeIcon icon={["fas", "trash-can"]} size="l"/>
+        <FontAwesomeIcon icon={["fas", "trash-can"]} size="sm"/>
       </button>
     </li>
   );
