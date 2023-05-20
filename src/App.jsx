aec7647f9ee4ad966e187fab615cf54632621c59
Saul Castillo <secch97@gmail.com>
2023-05-20 14:45:32 -0400

    Added new route (/new) which contains a <h1> text

--- src/App.jsx
+++ src/App.jsx
@@ -168,6 +168,11 @@ const App = () => {
             )}
           > 
           </Route>
+          <Route
+            path="/new"
+            element={(<h1>New Todo List</h1>)}
+          >
+          </Route>
         </Routes>
       </BrowserRouter>
     );
