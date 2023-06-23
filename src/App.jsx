ee6d18e76a370500db0839dc89fe08beab92b50f
Saul Castillo <secch97@gmail.com>
2023-06-23 16:17:31 -0600

    Sort by airtable field

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ const App = () => {
     ============================
   */
   const fetchData = async () => {
-    const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${process.env.REACT_APP_TABLE_NAME}?view=Grid%20view`;
+    const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${process.env.REACT_APP_TABLE_NAME}?sort[0][field]=title&sort[0][direction]=asc`;
     const options = {
       method:"GET",
       headers: {
